79d066013950509dd6b2f5b0cae1a2766030ad2a
Jonathan Marsh <hello@jonmarsh.dev>
2018-03-01 18:06:47 +0000

    Added auth

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var listenOnPort = 8084,
     logger = require('morgan'),
     bodyParser = require('body-parser'),
     path = require('path'),
+    request = require('request'),
+    url = require('url'),
     git = require("nodegit"),
     glob = require("glob"),
     compat = require("./compat.js");
@@ -28,8 +30,21 @@ app.get('/login', function (req, res) {
     res.redirect('https://github.com/login/oauth/authorize?client_id=' + process.env.CLIENT_ID + '&scope=repo');
 });
 app.get('/list', function (req, res) {
-	console.log(req);
-    res.render('list.html');
+    var query = url.parse(req.url, true).query;
+    if (query.code) {
+        var u = 'https://github.com/login/oauth/access_token'
+            + '?client_id=' + process.env.CLIENT_ID
+            + '&client_secret=' + process.env.CLIENT_SECRET
+            + '&code=' + query.code;
+        request.get({ url: u, json: true }, function (error, response, body) {
+            console.log("ERROR:",error);
+            console.log("RESPONSE:",response);
+            console.log("BODY",body);
+        });
+        res.render('list.html');
+    } else {
+        console.log("ERROR: No code returned");
+    }
 });
 app.get('/assess', function (req, res) {
     /*Insert old logic*/
