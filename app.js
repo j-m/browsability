3b2859b07e69a742648b047946d855e20deee86f
Jonathan Marsh <j.r.marsh@ntlworld.com>
2018-03-01 17:24:09 +0000

    Changed to dotenv-safe

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /*==Includes & variables==*/
-require('dotenv').config();
+require('dotenv-safe').config();
 var listenOnPort = 8084,
     express = require('express'),
     app = express(),
@@ -13,14 +13,12 @@ var listenOnPort = 8084,
     glob = require("glob"),
     compat = require("./compat.js");
 
-
 /*===Express app config===*/
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json());
 app.set('port', process.env.PORT || listenOnPort);
 http.listen(app.get('port'));
-
 
 /*=========Routes=========*/
 app.get('/', function (req, res) {
@@ -30,6 +28,7 @@ app.get('/login', function (req, res) {
     res.redirect('https://github.com/login/oauth/authorize?client_id=' + process.env.CLIENT_ID + '&scope=repo');
 });
 app.get('/list', function (req, res) {
+	console.log(req);
     res.render('list.html');
 });
 app.get('/assess', function (req, res) {
