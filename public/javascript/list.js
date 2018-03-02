c9a4029fd864df2abf592e9ee9817ed8d9b08f19
Jonathan Marsh <j.r.marsh@ntlworld.com>
2018-03-02 01:00:29 +0000

    File content retrieval working for public repos

--- public/javascript/list.js
+++ public/javascript/list.js
@@ -1,19 +1,40 @@
-﻿$(function () {
-    var url = new URL(window.location.href);
-    var token = url.searchParams.get("token");
-    if (token) {
-        var repouri = 'https://api.github.com/user/repos?access_token=' + token;
-        $.getJSON(repouri, function (repositories) {
-            var outhtml = "";
-            if (repositories.length == 0) { outhtml = outhtml + '<p>No repos!</p></div>'; }
-            else {
-                outhtml = outhtml + '<p><strong>Repos List:</strong></p> <ul>';
-                $.each(repositories, function (index) {
-                    outhtml = outhtml + '<li><a href="' + repositories[index].html_url + '" target="_blank">' + repositories[index].name + '</a></li>';
-                });
-                outhtml = outhtml + '</ul></div>';
-            }
-            $('#repos').html(outhtml);
+﻿var token = (new URL(window.location.href)).searchParams.get("token"),
+    files = [];
+function getContents(name, path) {
+    var uri = 'https://raw.githubusercontent.com/' + name + '/master/' + path + '?access_token=' + token;
+    $.get(uri, function (data) {
+        files.push({ file: path, content: data });
+    });
+}
+function getTree(name) {
+    $.getJSON('https://api.github.com/repos/' + name + '/commits/master?access_token=' + token, function (latest) { //get latest commit on master
+        $.getJSON('https://api.github.com/repos/' + name + '/git/trees/' + latest.sha + '?recursive=1&access_token=' + token, function (data) { //get tree object with paths
+            $.each(data.tree, function (index) {
+                var file = data.tree[index];
+                if (file.type == "blob") { //we only care about files
+                    var fileType = file.path.substr(file.path.lastIndexOf("."));
+                    if (fileType == ".browsability")
+                        getContents(name, file.path);
+                    else if (fileType == ".css")
+                        getContents(name, file.path);
+                    else if (fileType == ".html")
+                        getContents(name, file.path);
+                }
+            });
+            console.log(files);
         });
-    }
-});
+    });
+}
+if (token)
+    $.getJSON('https://api.github.com/user/repos?access_token=' + token, function (repositories) {
+        if (repositories.length == 0)
+            $('#repos').html('<p>No repos!</p></div>');
+        else {
+            var outhtml = '<p><strong>Repos List:</strong></p> <ul>';
+            $.each(repositories, function (index) {
+                outhtml += '<li><a href="#" onclick="getTree(&apos;' + repositories[index].full_name + '&apos;)">' + repositories[index].full_name + '</a></li>';
+            });
+            outhtml = outhtml + '</ul></div>';
+            $('#repos').html(outhtml);
+        }
+    });
