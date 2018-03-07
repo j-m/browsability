c62950ab0413c441499796bf963b8623207826e9
Jonathan Marsh <j.r.marsh@ntlworld.com>
2018-03-07 21:23:17 +0000

    Update config.js

--- public/javascript/config.js
+++ public/javascript/config.js
@@ -1,14 +1,17 @@
 var config = {
-	strict:false, /*partial support counts as supported*/
+	strict:false, 				/* False: partial support counts as supported; True: doesn't count*/
+	css: true, 
+	html: false, 				/* Coming soon*/
+	javascript: false,			/* Coming soon*/
 	files: [
-		".html", ".css",
-		exclusions: [], /*exact file names (with path from root) to exclude from the scan*/
-		inclusions: []	/*exact file names (with path from root) to additionally include in the scan*/
+		".html", ".css"
 	],
+	exclusions: [], 			/* Exact file names (with path from root) to exclude from the scan*/
+	inclusions: [],				/* Additional inclusions - if file-type is already listed, it will be scanned twice - which is a waste of your time)*/
 	browsers: {
 		"chrome": 62, 
-		"firefox", /*versions are not required; interpreted as latest*/
-		"safari", 
+		"firefox", 				/* Versions are not required; will tell you the minimum version supported */
+		"safari": "current", 	/* Matches the 'status' attributes in `mdn/browser-compat-data/browsers` */
 		"edge": 16, 
 		"ie": 11
 	}
