257e408ffbc7b37cdac5f27790a63e3ea054f293
Jonathan Marsh <hello@jonmarsh.dev>
2020-06-18 17:36:34 +0100

    BROW-8: Remove oclif (too many issues). Start work on own CLI

---
+++ src/command/version.ts
@@ -0,0 +1,3 @@
+export default function(args: Array<string>) {
+  console.log("0.1.0")
+}
--- src/parse/config.ts
+++ src/parse/config.ts
@@ -1,11 +1,8 @@
 import { parse } from 'hjson'
 import { readFileSync } from 'fs'
 
-
 type BrowserVersion = "any" | "current" | number
-
 type Browsers = {[key: string]: BrowserVersion}
-
 interface configuration {
   strict: boolean
   css: boolean     
@@ -16,7 +13,8 @@ interface configuration {
   browsers: Browsers
 }
 
-export default function(file: string) {
+export function load(file: string): configuration {
   const content: string = readFileSync(file,'utf8')
   const config: configuration = parse(content)
+  return config
 }
