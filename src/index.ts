4a71c404a8aae29f94a42e732ccfe764e91c0faf
Jonathan Marsh <hello@jonmarsh.dev>
2021-07-25 00:21:55 +0100

    feat(cli): correct entry

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
-import { parse } from './command/parse'
+import { BrowsabilityConfiguration } from 'config/Configuration'
+import { cli } from './cli/parse'
 
-export default function browsability(argv?: string[]): void {
-  const [, , ...args] = process.argv
-  parse(argv || args).catch((error) => {
+export default function browsability(config: BrowsabilityConfiguration[], cwd: string): void {
+  assess(config, cwd)
+}
+
+if (require.main === module) {
+  try {
+    const [, , ...args] = process.argv
+    cli(args)
+  } catch (error) {
     console.error(error)
     process.exitCode = 1
-  })
+  }
 }
